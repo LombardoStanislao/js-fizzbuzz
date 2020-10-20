0b3c53c9ebc719602527c53d9901c1fd8fd9d0b6
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-20 17:22:50 +0200

    Finito con array

--- Versione2/main.js
+++ Versione2/main.js
@@ -4,27 +4,32 @@
 // al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 
 var array = [];
+
+for (var i = 0; i < 100; i++) {
+    array[i] = (i+1);
+}
+
 // al posto dei multipli di 3, deve stampare "Fizz"
-for (var i = 1; i <= 100; i++) {
+for (var i = 0; i < array.length; i++) {
 
 
-    if ((i % 3 === 0) && (i % 5 === 0))  {
+    if ((array[i] % 3 == 0) && (array[i] % 5 == 0))  {
 
-        array[i]= 'FizzBuzz'
+        console.log(array[i]= 'FizzBuzz');
 
-    } else if (i % 5 === 0) {
+    } else if (array[i] % 5 == 0) {
 
-        array[i]= 'Buzz'
+        console.log(array[i]= 'Buzz');
 
-    } else if (i % 3 === 0) {
+    } else if (array[i] % 3 == 0) {
 
-         array[i]= 'Fizz'
+        console.log( array[i]= 'Fizz');
 
     } else {
 
-        array[i]
+        console.log(array[i]);
     }
 
 }
-
-console.log(array);
+//
+// console.log(array);
