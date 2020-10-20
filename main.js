0949a7c2d92687cd8cccf168c7558c28c737a178
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-20 15:51:09 +0200

    ticket

--- main.js
+++ main.js
@@ -3,6 +3,25 @@
 // al posto dei multipli di 5, deve stampare "Buzz"
 // al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 
+var fizz;
+var buzz;
+var fizzBuzz;
+// Creo un array vuoto da riempir di numeri da 1 a 100
+var array = [];
+
+
+// al posto dei multipli di 3, deve stampare "Fizz"
 for (var i = 1; i <= 100; i++) {
-    console.log(i);
+
+
+    if (i % 3 === 0) {
+        i = 'Fizz';
+    } else if (i % 5 === 0) {
+        i = 'Buzz'
+    } else if ((i % 3 === 0) && (i % 5 === 0))  {
+
+        i = 'FizzBuzz'
+    }
+
+        console.log(i);
 }
